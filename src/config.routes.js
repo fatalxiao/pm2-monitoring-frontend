633a4c340ddf5ae55455fe534d9c2f5c3161f72e
liangxiaojun <fatalxiao@163.com>
2018-04-18 22:51:51 +0800

    update files

--- src/config.routes.js
+++ src/config.routes.js
@@ -5,15 +5,11 @@ export function configureRoutes(store) {
     return [{
         component: ac(() => import('containers/AppRoot'), store),
         routes: [{
-            path: '/app',
-            component: ac(() => import('containers/app/App'), store),
-            routes: [{
-                path: '/app/process/:id',
-                component: ac(() => import('containers/app/modules/process/Process'), store)
-            }]
+            path: '/process/:id',
+            component: ac(() => import('containers/process/Process'), store)
         }]
     }];
 
 }
 
-export const DEFAULT_ROUTE = '/app';
+export const DEFAULT_ROUTE = '/';
