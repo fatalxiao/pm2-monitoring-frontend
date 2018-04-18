8dece5c809fc38993ef8124c05383e3b6ec56f0f
liangxiaojun <fatalxiao@163.com>
2018-04-18 22:31:45 +0800

    update files

--- src/containers/AppRoot.js
+++ src/containers/AppRoot.js
@@ -14,7 +14,6 @@ import {DEFAULT_ROUTE} from 'src/config.routes';
 
 import 'assets/bootstrap/bootstrap-grid.min.css';
 import 'assets/font-awesome/css/fontawesome-all.min.css';
-import 'assets/icomoon/style.css';
 import 'scss/customized/index.scss';
 import 'scss/global.scss';
 import 'scss/containers/AppRoot.scss';
