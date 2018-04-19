84982f8e15f04c63a6aa3705097ed16502521793
liangxiaojun <fatalxiao@163.com>
2018-04-19 15:43:51 +0800

    update files

--- src/containers/AppRoot.js
+++ src/containers/AppRoot.js
@@ -3,15 +3,12 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {renderRoutes} from 'react-router-config';
-import {Redirect} from 'react-router-dom';
 
 import * as actions from 'reduxes/actions';
 
 import Toaster from 'alcedo-ui/Toaster';
 import Notifier from 'alcedo-ui/Notifier';
 import App from 'containers/app/App';
-
-import {DEFAULT_ROUTE} from 'src/config.routes';
 
 import 'assets/bootstrap/bootstrap-grid.min.css';
 import 'assets/font-awesome/css/fontawesome-all.min.css';
@@ -22,16 +19,32 @@ import 'scss/containers/AppRoot.scss';
 class AppRoot extends Component {
 
     constructor(props) {
+
         super(props);
+
+        this.ws = new WebSocket('ws://localhost:9616/pm/monitoring');
+
     }
 
     componentDidMount() {
+
         this.props.submitAsyncMsgSeq();
+
+        this.ws.onopen = e => {
+            console.log('Connection open ...');
+            this.ws.send('Hello WebSockets!');
+        };
+
+        this.ws.onmessage = e => {
+            console.log('Received Message: ' + e.data);
+            this.ws.close();
+        };
+
     }
 
     render() {
 
-        const {$toastes, $notifiers, route, location, clearToaste, clearNotifier} = this.props;
+        const {$toastes, $notifiers, route, clearToaste, clearNotifier} = this.props;
 
         return (
             <div className="app-root">
