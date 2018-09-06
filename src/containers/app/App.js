ef1d2e97d7ac842aa407ef9c939fe4ac41e99a64
liangxiaojun <fatalxiao@163.com>
2018-09-06 22:35:58 +0800

    update App

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -2,10 +2,13 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import {renderRoutes} from 'react-router-config';
 
 import * as actions from 'reduxes/actions';
 
-import Processes from './processes/Processes';
+import PageLoading from 'alcedo-ui/PageLoading';
+
+import Dom from 'vendors/Dom';
 
 import 'scss/containers/app/App.scss';
 
@@ -16,22 +19,42 @@ class App extends Component {
     }
 
     componentDidMount() {
-
+        Dom.removeClass(document.querySelector('html'), 'full-size');
     }
 
     render() {
+
+        const {route, componentLoading} = this.props;
+
         return (
             <div className="app">
 
-                <Processes/>
+                <div ref="appContent"
+                     className="app-content">
+
+                    <PageLoading visible={componentLoading}
+                                 showStripes={false}/>
+
+                    {renderRoutes(route.routes)}
+
+                </div>
 
             </div>
         );
+
     }
 }
 
 App.propTypes = {
-    getCurrentMonitoringData: PropTypes.func
+
+    componentLoading: PropTypes.bool,
+
+    routerPush: PropTypes.func
+
 };
 
-export default connect(state => ({}), dispatch => bindActionCreators(actions, dispatch))(App);
+export default connect(state => ({
+    componentLoading: state.loadComponent.loading
+}), dispatch => bindActionCreators({
+    routerPush: actions.routerPush
+}, dispatch))(App);
