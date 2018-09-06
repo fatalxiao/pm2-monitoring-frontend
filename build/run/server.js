d426539f0fe5e4b9d426cf9d7d22daa15b3e0618
liangxiaojun <fatalxiao@163.com>
2018-09-06 22:32:40 +0800

    update webpack run build

--- build/run/server.js
+++ build/run/server.js
@@ -21,7 +21,8 @@ Object.keys(proxyTable).forEach(context => {
     if (typeof options === 'string') {
         options = {
             target: options,
-            changeOrigin: true
+            changeOrigin: true,
+            logLevel: 'error'
         };
     }
 
@@ -46,10 +47,13 @@ app
 .use(express.static(config[env].assetsRoot, {
     setHeaders: (res, path) => {
         res.setHeader('Cache-Control', path.endsWith('index.html') ?
-            'no-cache, no-store, no_store, max-age=0, must-revalidate' : 'max-age=2592000'
+            'no-cache, no-store, no_store, max-age=0, must-revalidate' : 'max-age=315360000'
         );
     }
 }))
+.get('/derby_forever_health/status.ci', (req, res) => {
+    res.sendStatus(200);
+})
 .listen(port, err => {
 
     if (err) {
