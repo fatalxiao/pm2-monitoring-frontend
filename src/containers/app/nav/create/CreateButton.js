e75a31997dd452f52562a380ce9cd72889e9206d
liangxiaojun <fatalxiao@163.com>
2018-09-07 20:10:52 +0800

    update Nav

--- src/containers/app/nav/create/CreateButton.js
+++ src/containers/app/nav/create/CreateButton.js
@@ -2,20 +2,73 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import classNames from 'classnames';
 
 import * as actions from 'reduxes/actions';
+
+import Util from 'vendors/Util';
 
 import 'scss/containers/app/nav/create/CreateButton.scss';
 
 class CreateButton extends Component {
 
     constructor(props) {
+
         super(props);
+
+        this.defaultSize = 56;
+
+        this.state = {
+            avtivated: false,
+            size: this.defaultSize
+        };
+
+    }
+
+    toggle = () => {
+
+        if (this.state.avtivated || !this.buttonEl) {
+            return;
+        }
+
+        const pointer = Util.getElCenterPoint(this.buttonEl);
+
+        if (!pointer) {
+            return;
+        }
+
+        const {x, y} = pointer,
+            windowWidth = window.innerWidth,
+            windowHeight = window.innerHeight;
+
+        this.setState({
+            avtivated: true,
+            size: Util.getDiag(windowWidth, windowHeight)
+        });
+
+    };
+
+    componentDidMount() {
+        this.buttonEl = this.refs.button;
     }
 
     render() {
+
+        const {avtivated, size} = this.state,
+
+            className = classNames('create-button', {
+                avtivated
+            }),
+            style = {
+                width: size,
+                height: size
+            };
+
         return (
-            <div className="create-button">
+            <div ref="button"
+                 className={className}
+                 style={style}
+                 onClick={this.toggle}>
 
             </div>
         );
