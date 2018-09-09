43ead177a7e5aa1be8a06a3695d3b636b0e6cea1
liangxiaojun <fatalxiao@163.com>
2018-09-09 16:09:41 +0800

    update routeParam

--- src/reduxes/reducers/common/RouteParamReducer.js
+++ src/reduxes/reducers/common/RouteParamReducer.js
@@ -1,78 +1,12 @@
-import * as types from 'reduxes/actionTypes/index';
+import * as types from 'reduxes/actionTypes';
 
-const initialState = {
-    report: {
-        id: '',
-        name: ''
-    },
-    campaign: {
-        id: '',
-        name: '',
-        strategy: null
-    },
-    campaignGroup: {
-        id: '',
-        name: ''
-    },
-    campaignHotel: {
-        id: '',
-        name: ''
-    }
-};
+const initialState = {};
 
 function routeParam(state = initialState, action) {
 
     switch (action.type) {
-
-        case types.UPDATE_ROUTE_PARAM_REPORT: {
-            return {
-                ...state,
-                report: {
-                    ...state.report,
-                    ...action.report
-                }
-            };
-        }
-
-        case types.UPDATE_ROUTE_PARAM_CAMPAIGN: {
-            return {
-                ...state,
-                campaign: {
-                    ...state.campaign,
-                    ...action.campaign
-                }
-            };
-        }
-
-        case types.UPDATE_ROUTE_PARAM_CAMPAIGN_GROUP: {
-            return {
-                ...state,
-                campaignGroup: action.campaignGroup
-            };
-        }
-
-        case types.UPDATE_ROUTE_PARAM_CAMPAIGN_HOTEL: {
-            return {
-                ...state,
-                campaignHotel: action.campaignHotel
-            };
-        }
-
-        case types.UPDATE_ROUTE_PARAM_CAMPAIGN_STRATEGY: {
-            const {strategy} = action;
-
-            return {
-                ...state,
-                campaign: {
-                    ...state.campaign,
-                    strategy
-                }
-            };
-        }
-
         default:
             return state;
-
     }
 }
 
