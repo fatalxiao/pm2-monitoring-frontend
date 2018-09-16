6a938e6ac0f58a896730f6f971ff1c54d285cc86
liangxiaojun <fatalxiao@163.com>
2018-09-16 15:11:07 +0800

    update Process

--- src/containers/app/processes/Process.js
+++ src/containers/app/processes/Process.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import classNames from 'classnames';
 
 import * as actions from 'reduxes/actions';
 
@@ -15,11 +16,31 @@ class Process extends Component {
 
     render() {
 
-        const {data} = this.props;
+        const {data} = this.props,
+            activated = 'pm_id' in data,
+            processClassName = classNames('col-lg-3 col-md-6 col-sm-6 col-xs-12 process', {
+                activated
+            });
 
         return (
-            <div className="process">
-                {data.name}
+            <div className={processClassName}>
+
+                <div className="process-name">{data.name}</div>
+
+                {
+                    activated ?
+                        <div className="process-monit">
+                            <div className="process-monit-cpu">
+                                {`CPU: ${data.monit ? data.monit.cpu : 0}`}
+                            </div>
+                            <div className="process-monit-memory">
+                                {`Memory: ${data.monit ? data.monit.memory : 0}`}
+                            </div>
+                        </div>
+                        :
+                        null
+                }
+
             </div>
         );
     }
