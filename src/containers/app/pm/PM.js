7feefba3e8cb433669a672ed300120d3a5bab58f
liangxiaojun <fatalxiao@163.com>
2018-09-30 09:52:53 +0800

    update PM

--- src/containers/app/pm/PM.js
+++ src/containers/app/pm/PM.js
@@ -2,9 +2,12 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import classNames from 'classnames';
 
 import * as actions from 'reduxes/actions';
+import * as actionTypes from 'reduxes/actionTypes';
 
+import CircularLoading from 'alcedo-ui/CircularLoading';
 import Nav from './nav/Nav';
 import Applications from './applications/Applications';
 
@@ -22,19 +25,40 @@ class PM extends Component {
     }
 
     render() {
+
+        const {init, getApplicationsActionType} = this.props,
+
+            loadClassName = classNames('loading-wrapper', {
+                activated: init && (
+                    getApplicationsActionType === actionTypes.GET_APPLICATIONS_REQUEST
+                    || getApplicationsActionType === actionTypes.GET_APPLICATIONS_FAILURE
+                )
+            });
+
         return (
             <div className="pm">
+
                 <Nav/>
                 <Applications/>
+
+                <div className={loadClassName}>
+                    <CircularLoading/>
+                </div>
+
             </div>
         );
     }
 }
 
 PM.propTypes = {
+    init: PropTypes.bool,
+    getApplicationsActionType: PropTypes.string,
     runGetApplicationsInterval: PropTypes.func
 };
 
-export default connect(state => ({}), dispatch => bindActionCreators({
+export default connect(state => ({
+    init: state.applications.init,
+    getApplicationsActionType: state.applications.actionType
+}), dispatch => bindActionCreators({
     runGetApplicationsInterval: actions.runGetApplicationsInterval
 }, dispatch))(PM);
