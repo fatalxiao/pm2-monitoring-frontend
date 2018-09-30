1771906d4db28f5900483c4efaa86c96bf1c941e
liangxiaojun <fatalxiao@163.com>
2018-09-30 11:03:02 +0800

    update CreateForm

--- src/containers/app/pm/nav/create/CreateForm.js
+++ src/containers/app/pm/nav/create/CreateForm.js
@@ -4,9 +4,10 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import classNames from 'classnames';
 
-import * as actions from 'reduxes/actions/index';
+import * as actions from 'reduxes/actions';
 
 import TextField from 'alcedo-ui/MaterialTextField';
+import TextArea from 'alcedo-ui/MaterialTextArea';
 
 import 'scss/containers/app/pm/nav/create/CreateForm.scss';
 
@@ -33,7 +34,20 @@ class CreateForm extends Component {
                     <TextField className="form-field"
                                label="Application Name"
                                isLabelAnimate={false}
-                               placeholder="new-application"/>
+                               placeholder="new-application"
+                               clearButtonVisible={false}/>
+
+                    <TextArea className="form-field"
+                              label="Description"
+                              isLabelAnimate={false}
+                              clearButtonVisible={false}
+                              autoHeight={true}/>
+
+                    <TextField className="form-field"
+                               label="Script"
+                               isLabelAnimate={false}
+                               placeholder="server.js"
+                               clearButtonVisible={false}/>
 
                 </div>
             </div>
