2ede22b95a733e58a392fe410ea0afae1dc102a0
liangxiaojun <fatalxiao@163.com>
2018-10-03 22:39:40 +0800

    update ApplicationReducer

--- src/reduxes/reducers/app/ApplicationReducer.js
+++ src/reduxes/reducers/app/ApplicationReducer.js
@@ -1,63 +1,21 @@
 import * as actionTypes from 'reduxes/actionTypes';
 
 const initialState = {
-    data: {}
+    actionType: {}
 };
 
 function application(state = initialState, action) {
-
-    // switch (action.type) {
-    //
-    //     // start application
-    //     case actionTypes.START_APPLICATION_REQUEST: {
-    //
-    //         const data = state.data;
-    //         data[action.apiParams.applicationId] = actionTypes.START_APPLICATION_REQUEST;
-    //
-    //         return {
-    //             ...state,
-    //             data
-    //         };
-    //
-    //     }
-    //     case actionTypes.START_APPLICATION_SUCCESS: {
-    //
-    //         const data = state.data;
-    //         delete data[action.apiParams.applicationId];
-    //
-    //         return {
-    //             ...state,
-    //             data
-    //         };
-    //
-    //     }
-    //     case actionTypes.START_APPLICATION_FAILURE: {
-    //
-    //         const data = state.data;
-    //         delete data[action.apiParams.applicationId];
-    //
-    //         return {
-    //             ...state,
-    //             data
-    //         };
-    //
-    //     }
-    //
-    //     default:
-    //         return state;
-    //
-    // }
 
     if (action.type === actionTypes.START_APPLICATION_REQUEST
         || action.type === actionTypes.PAUSE_APPLICATION_REQUEST
         || action.type === actionTypes.RESTART_APPLICATION_REQUEST
         || action.type === actionTypes.STOP_APPLICATION_REQUEST
         || action.type === actionTypes.RELOAD_APPLICATION_REQUEST) {
-        const data = state.data;
-        data[action.apiParams.applicationId] = action.type;
+        const actionType = state.actionType;
+        actionType[action.apiParams.applicationId] = action.type;
         return {
             ...state,
-            data
+            actionType
         };
     } else if (action.type === actionTypes.START_APPLICATION_SUCCESS
         || action.type === actionTypes.PAUSE_APPLICATION_SUCCESS
@@ -69,11 +27,11 @@ function application(state = initialState, action) {
         || action.type === actionTypes.RESTART_APPLICATION_FAILURE
         || action.type === actionTypes.STOP_APPLICATION_FAILURE
         || action.type === actionTypes.RELOAD_APPLICATION_FAILURE) {
-        const data = state.data;
-        delete data[action.apiParams.applicationId];
+        const actionType = state.actionType;
+        delete actionType[action.apiParams.applicationId];
         return {
             ...state,
-            data
+            actionType
         };
     }
 
