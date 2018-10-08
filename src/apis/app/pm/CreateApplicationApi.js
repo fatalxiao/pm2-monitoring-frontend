69bd4b39fc32dfb22c90dae82dd725afd30b2287
liangxiaojun <fatalxiao@163.com>
2018-10-08 11:08:03 +0800

    update CreateApplicationApi

--- src/apis/app/pm/CreateApplicationApi.js
+++ src/apis/app/pm/CreateApplicationApi.js
@@ -4,7 +4,7 @@ import config from '../../../config';
 export default {
 
     createApplication(options) {
-        Api.put({
+        Api.post({
             ...options,
             url: `${config.baseUrl}/application/create`
         });
