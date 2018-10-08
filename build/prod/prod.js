3a71fa474406741fe078afe0d121067dd35ff285
liangxiaojun <fatalxiao@163.com>
2018-10-08 15:42:55 +0800

    update build prod

--- build/prod/prod.js
+++ build/prod/prod.js
@@ -1,17 +1,7 @@
-const fs = require('fs'),
-    webpack = require('webpack'),
-    archiver = require('archiver'),
-    crypto = require('crypto'),
+const webpack = require('webpack'),
     log = require('friendly-errors-webpack-plugin/src/output'),
 
-    config = require('../config.js'),
-    webpackConfig = require('./webpack.config.prod.js'),
-    {fsExistsSync, copyRecursionSync, rmRecursionSync} = require('../utils.js'),
-
-    env = process.env.NODE_ENV,
-    name = env === 'production' ? 'dplatform-click-web' : `dplatform-click-web-${env}`,
-    path = `./${name}`,
-    zipPath = `./${name}.zip`;
+    webpackConfig = require('./webpack.config.prod.js');
 
 log.title('info', 'WAIT', 'Building Production...');
 
@@ -29,51 +19,6 @@ webpack(webpackConfig, (err, stats) => {
         chunkModules: false
     }) + '\n\n');
 
-    // remove zip file
-    if (fsExistsSync(zipPath)) {
-        fs.unlinkSync(zipPath);
-    }
-
-    // remove temp dir
-    if (fsExistsSync(path)) {
-        rmRecursionSync(path);
-    }
-
-    // make temp dir
-    fs.mkdirSync(path);
-
-    // copy files
-    copyRecursionSync(config[env].rootDirectory, path, ['node_modules', '.DS_Store']);
-    copyRecursionSync('./release/server', path);
-    copyRecursionSync('./release/shell', path);
-
-    // make archive
-    const output = fs.createWriteStream(zipPath),
-        archive = archiver('zip', {zlib: {level: 9}});
-
-    output.on('close', () => {
-
-        // remove temp dir
-        if (fsExistsSync(path)) {
-            rmRecursionSync(path);
-        }
-
-        // calculate SHA-256 Hash
-        const rs = fs.createReadStream(zipPath),
-            hash = crypto.createHash('sha256');
-
-        rs.on('data', hash.update.bind(hash));
-        rs.on('end', function () {
-            log.title('success', 'DONE', [
-                'Build Production complete',
-                `Archive: ${archive.pointer()} total bytes`,
-                `SHA-256 Hash: ${hash.digest('hex')}`
-            ].join('\n       '));
-        });
-
-    });
-    archive.pipe(output);
-    archive.directory(path, name);
-    archive.finalize();
+    log.title('success', 'DONE', `Build Complete `);
 
 });
