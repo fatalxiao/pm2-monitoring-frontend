1a35e035cdb9ea52234f85687490a1177088b6c9
liangxiaojun <fatalxiao@163.com>
2018-10-10 20:55:35 +0800

    update app config

--- src/config.js
+++ src/config.js
@@ -3,6 +3,11 @@ export default {
     desktopMinWidth: 768,
 
     /**
+     * request interval of pm2 list websocket
+     */
+    refreshInterval: 5,
+
+    /**
      * Api base url prefix
      */
     baseUrl: '/pm',
