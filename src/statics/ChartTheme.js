99071b2cc826f7c9a65b351770ce61fdb493ea16
liangxiaojun <fatalxiao@163.com>
2018-10-16 20:20:41 +0800

    update ChartTheme

--- src/statics/ChartTheme.js
+++ src/statics/ChartTheme.js
@@ -1,134 +1,61 @@
-var contrastColor = '#eee';
-var axisCommon = function () {
-    return {
-        axisLine: {
-            lineStyle: {
-                color: contrastColor
-            }
-        },
-        axisTick: {
-            lineStyle: {
-                color: contrastColor
-            }
-        },
-        axisLabel: {
-            textStyle: {
-                color: contrastColor
-            }
-        },
-        splitLine: {
-            lineStyle: {
-                type: 'dashed',
-                color: '#aaa'
-            }
-        },
-        splitArea: {
-            areaStyle: {
-                color: contrastColor
-            }
-        }
-    };
-};
-
 var colorPalette = [
-    '#dd6b66', '#759aa0', '#e69d87', '#8dc1a9', '#ea7e53',
-    '#eedd78', '#73a373', '#73b9bc', '#7289ab', '#91ca8c', '#f49f42'
+    '#488fdf', '#e6b600', '#0098d9', '#2b821d',
+    '#005eaa', '#339ca8', '#cda819', '#32a487'
 ];
-var theme = {
+
+export default {
+
     color: colorPalette,
-    backgroundColor: '#333',
-    tooltip: {
-        axisPointer: {
-            lineStyle: {
-                color: contrastColor
-            },
-            crossStyle: {
-                color: contrastColor
-            }
-        }
+
+    visualMap: {
+        color: ['#1790cf', '#a2d4e6']
     },
-    legend: {
-        textStyle: {
-            color: contrastColor
-        }
-    },
-    textStyle: {
-        color: contrastColor
-    },
-    title: {
-        textStyle: {
-            color: contrastColor
-        }
-    },
+
     toolbox: {
         iconStyle: {
             normal: {
-                borderColor: contrastColor
+                borderColor: '#06467c'
             }
         }
     },
+
+    tooltip: {
+        backgroundColor: 'rgba(0,0,0,0.6)'
+    },
+
     dataZoom: {
-        textStyle: {
-            color: contrastColor
-        }
+        dataBackgroundColor: '#dedede',
+        fillerColor: 'rgba(154,217,247,0.2)',
+        handleColor: '#005eaa'
     },
-    visualMap: {
-        textStyle: {
-            color: contrastColor
-        }
-    },
+
     timeline: {
         lineStyle: {
-            color: contrastColor
-        },
-        itemStyle: {
-            normal: {
-                color: colorPalette[1]
-            }
-        },
-        label: {
-            normal: {
-                textStyle: {
-                    color: contrastColor
-                }
-            }
+            color: '#005eaa'
         },
         controlStyle: {
             normal: {
-                color: contrastColor,
-                borderColor: contrastColor
+                color: '#005eaa',
+                borderColor: '#005eaa'
             }
         }
     },
-    timeAxis: axisCommon(),
-    logAxis: axisCommon(),
-    valueAxis: axisCommon(),
-    categoryAxis: axisCommon(),
 
-    line: {
-        symbol: 'circle'
-    },
-    graph: {
-        color: colorPalette
-    },
-    gauge: {
-        title: {
-            textStyle: {
-                color: contrastColor
-            }
-        }
-    },
     candlestick: {
         itemStyle: {
             normal: {
-                color: '#FD1050',
-                color0: '#0CF49B',
-                borderColor: '#FD1050',
-                borderColor0: '#0CF49B'
+                color: '#c12e34',
+                color0: '#2b821d',
+                lineStyle: {
+                    width: 1,
+                    color: '#c12e34',
+                    color0: '#2b821d'
+                }
             }
         }
+    },
+
+    graph: {
+        color: colorPalette
     }
 };
-theme.categoryAxis.splitLine.show = false;
-
-export default theme;
