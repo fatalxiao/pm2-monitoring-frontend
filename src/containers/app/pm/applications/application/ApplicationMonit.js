fe919b9bd892ce3978c32b8fa7b6273adaaee6e2
liangxiaojun <fatalxiao@163.com>
2018-10-16 20:27:41 +0800

    update ApplicationMonit

--- src/containers/app/pm/applications/application/ApplicationMonit.js
+++ src/containers/app/pm/applications/application/ApplicationMonit.js
@@ -17,34 +17,38 @@ class ApplicationMonit extends Component {
     render() {
 
         const {data, status} = this.props,
+            activated = status === 'online',
 
-            cpu = status === 'online' && data.monit ?
-                Calculation.formatCPU(data.monit.cpu)
-                :
-                '--',
-
-            memory = status === 'online' && data.monit ?
-                Calculation.formatMemory(data.monit.memory)
-                :
-                '--';
+            cpuClassName = classNames('application-monit-value', {
+                activated
+            }),
+            memoryClassName = classNames('application-monit-value', {
+                activated
+            });
 
         return (
             <div className="application-monit">
                 <div className="application-monit-cpu">
                     <div className="application-monit-title">CPU</div>
-                    <div className={classNames('application-monit-value', {
-                        activated: status === 'online'
-                    })}>
-                        {cpu}
+                    <div className={cpuClassName}>
+                        {
+                            activated && data.monit ?
+                                Calculation.formatCPU(data.monit.cpu)
+                                :
+                                '--'
+                        }
                         <span className="application-monit-unit">%</span>
                     </div>
                 </div>
                 <div className="application-monit-memory">
                     <div className="application-monit-title">Memory</div>
-                    <div className={classNames('application-monit-value', {
-                        activated: status === 'online'
-                    })}>
-                        {memory}
+                    <div className={memoryClassName}>
+                        {
+                            activated && data.monit ?
+                                Calculation.formatMemory(data.monit.memory)
+                                :
+                                '--'
+                        }
                         <span className="application-monit-unit">MB</span>
                     </div>
                 </div>
