60d03e38560c039132735e585e478e536428bce1
liangxiaojun <fatalxiao@163.com>
2018-10-16 16:15:34 +0800

    update ApplicationOverView

--- src/containers/app/pm/applicationDetail/ApplicationOverView.js
+++ src/containers/app/pm/applicationDetail/ApplicationOverView.js
@@ -2,8 +2,12 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import ReactEcharts from 'echarts-for-react';
 
 import Status from '../common/ApplicationStatus';
+import LineChart from '../common/LineChart';
+
+import Calaulation from 'vendors/Calaulation';
 
 import 'scss/containers/app/pm/applicationDetail/ApplicationOverView.scss';
 
@@ -12,6 +16,37 @@ class ApplicationOverView extends Component {
     constructor(props) {
         super(props);
     }
+
+    formatData = type => {
+
+        const {data} = this.props;
+
+        if (!data || !data.monitRecord) {
+            return [];
+        }
+
+        return data.monitRecord.map(item => {
+
+            if (!item) {
+                return item;
+            }
+
+            const result = {
+                name: '' + item.time,
+                value: [
+                    item.time
+                ]
+            };
+
+            if (item[type] !== null) {
+                result.value.push(Calaulation.format(type, item[type]));
+            }
+
+            return result;
+
+        });
+
+    };
 
     render() {
 
@@ -23,6 +58,16 @@ class ApplicationOverView extends Component {
                 <div className="overview-item status">
                     <div className="overview-item-title">Status</div>
                     <Status value={data.status}/>
+                </div>
+
+                <div className="overview-item cpu">
+                    <div className="overview-item-title">CPU</div>
+                    <LineChart data={this.formatData('cpu')}/>
+                </div>
+
+                <div className="overview-item memory">
+                    <div className="overview-item-title">Memory</div>
+                    <LineChart data={this.formatData('memory')}/>
                 </div>
 
             </div>
