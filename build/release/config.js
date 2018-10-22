575d9336ab0028f9e9ff1dc90abfa12827a3ad73
liangxiaojun <fatalxiao@163.com>
2018-10-22 20:53:44 +0800

    update release config

--- build/release/config.js
+++ build/release/config.js
@@ -3,7 +3,8 @@ module.exports = {
     serverPort: '9617',
 
     proxyTable: {
-        '/pm': 'http://localhost:9616'
+        '/pm': 'http://localhost:9616',
+        '/ws/pm': 'ws://localhost:9616'
     }
 
 };
