a22c1ab48cb3ac120c273f23de3ee16ed4a7604b
liangxiaojun <fatalxiao@163.com>
2018-10-24 18:50:49 +0800

    update ApplicationReducer

--- src/reduxes/reducers/app/pm/ApplicationReducer.js
+++ src/reduxes/reducers/app/pm/ApplicationReducer.js
@@ -14,7 +14,8 @@ function application(state = initialState, action) {
         || action.type === actionTypes.PAUSE_APPLICATION_REQUEST
         || action.type === actionTypes.RESTART_APPLICATION_REQUEST
         || action.type === actionTypes.STOP_APPLICATION_REQUEST
-        || action.type === actionTypes.RELOAD_APPLICATION_REQUEST) {
+        || action.type === actionTypes.RELOAD_APPLICATION_REQUEST
+        || action.type === actionTypes.RENAME_APPLICATION_REQUEST) {
         const actionType = cloneDeep(state.actionType);
         actionType[action.apiParams.applicationName] = action.type;
         return {
@@ -28,13 +29,15 @@ function application(state = initialState, action) {
         || action.type === actionTypes.RESTART_APPLICATION_SUCCESS
         || action.type === actionTypes.STOP_APPLICATION_SUCCESS
         || action.type === actionTypes.RELOAD_APPLICATION_SUCCESS
+        || action.type === actionTypes.RENAME_APPLICATION_SUCCESS
         || action.type === actionTypes.UPDATE_APPLICATION_FAILURE
         || action.type === actionTypes.UPLOAD_APPLICATION_PACKAGE_FAILURE
         || action.type === actionTypes.START_APPLICATION_FAILURE
         || action.type === actionTypes.PAUSE_APPLICATION_FAILURE
         || action.type === actionTypes.RESTART_APPLICATION_FAILURE
         || action.type === actionTypes.STOP_APPLICATION_FAILURE
-        || action.type === actionTypes.RELOAD_APPLICATION_FAILURE) {
+        || action.type === actionTypes.RELOAD_APPLICATION_FAILURE
+        || action.type === actionTypes.RENAME_APPLICATION_FAILURE) {
         const actionType = cloneDeep(state.actionType);
         delete actionType[action.apiParams.applicationName];
         return {
