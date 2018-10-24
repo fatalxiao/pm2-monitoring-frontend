ba57d660fdc27f8e761ae8ae0bfb1a0ef906ebad
liangxiaojun <fatalxiao@163.com>
2018-10-24 19:28:57 +0800

    update ApplicationReducer

--- src/reduxes/reducers/app/pm/ApplicationReducer.js
+++ src/reduxes/reducers/app/pm/ApplicationReducer.js
@@ -3,7 +3,8 @@ import cloneDeep from 'lodash/cloneDeep';
 import * as actionTypes from 'reduxes/actionTypes';
 
 const initialState = {
-    actionType: {}
+    actionType: {},
+    error: {}
 };
 
 function application(state = initialState, action) {
@@ -16,12 +17,19 @@ function application(state = initialState, action) {
         || action.type === actionTypes.STOP_APPLICATION_REQUEST
         || action.type === actionTypes.RELOAD_APPLICATION_REQUEST
         || action.type === actionTypes.RENAME_APPLICATION_REQUEST) {
-        const actionType = cloneDeep(state.actionType);
+
+        const actionType = cloneDeep(state.actionType),
+            error = cloneDeep(state.error);
+
         actionType[action.apiParams.applicationName] = action.type;
+        delete error[action.apiParams.applicationName];
+
         return {
             ...state,
-            actionType
+            actionType,
+            error
         };
+
     } else if (action.type === actionTypes.UPDATE_APPLICATION_SUCCESS
         || action.type === actionTypes.UPLOAD_APPLICATION_PACKAGE_SUCCESS
         || action.type === actionTypes.START_APPLICATION_SUCCESS
@@ -29,8 +37,17 @@ function application(state = initialState, action) {
         || action.type === actionTypes.RESTART_APPLICATION_SUCCESS
         || action.type === actionTypes.STOP_APPLICATION_SUCCESS
         || action.type === actionTypes.RELOAD_APPLICATION_SUCCESS
-        || action.type === actionTypes.RENAME_APPLICATION_SUCCESS
-        || action.type === actionTypes.UPDATE_APPLICATION_FAILURE
+        || action.type === actionTypes.RENAME_APPLICATION_SUCCESS) {
+
+        const actionType = cloneDeep(state.actionType);
+        delete actionType[action.apiParams.applicationName];
+
+        return {
+            ...state,
+            actionType
+        };
+
+    } else if (action.type === actionTypes.UPDATE_APPLICATION_FAILURE
         || action.type === actionTypes.UPLOAD_APPLICATION_PACKAGE_FAILURE
         || action.type === actionTypes.START_APPLICATION_FAILURE
         || action.type === actionTypes.PAUSE_APPLICATION_FAILURE
@@ -38,11 +55,17 @@ function application(state = initialState, action) {
         || action.type === actionTypes.STOP_APPLICATION_FAILURE
         || action.type === actionTypes.RELOAD_APPLICATION_FAILURE
         || action.type === actionTypes.RENAME_APPLICATION_FAILURE) {
-        const actionType = cloneDeep(state.actionType);
+
+        const actionType = cloneDeep(state.actionType),
+            error = cloneDeep(state.error);
+
         delete actionType[action.apiParams.applicationName];
+        delete error[action.apiParams.applicationName];
+
         return {
             ...state,
-            actionType
+            actionType,
+            error
         };
     }
 
