538bf1a0c8ec170fd89ddd3a02a33b5fef6fb00b
liangxiaojun <fatalxiao@163.com>
2018-11-06 20:00:39 +0800

    update ApplicationReducer

--- src/reduxes/reducers/app/pm/ApplicationReducer.js
+++ src/reduxes/reducers/app/pm/ApplicationReducer.js
@@ -11,11 +11,11 @@ function application(state = initialState, action) {
 
     if (action.type === actionTypes.UPDATE_APPLICATION_REQUEST
         || action.type === actionTypes.UPLOAD_APPLICATION_PACKAGE_REQUEST
-        || action.type === actionTypes.START_APPLICATION_REQUEST
-        || action.type === actionTypes.PAUSE_APPLICATION_REQUEST
-        || action.type === actionTypes.RESTART_APPLICATION_REQUEST
-        || action.type === actionTypes.STOP_APPLICATION_REQUEST
-        || action.type === actionTypes.RELOAD_APPLICATION_REQUEST
+        || action.type === actionTypes.START_APPLICATION_PROCESS_REQUEST
+        || action.type === actionTypes.STOP_APPLICATION_PROCESS_REQUEST
+        || action.type === actionTypes.RESTART_APPLICATION_PROCESS_REQUEST
+        || action.type === actionTypes.DELETE_APPLICATION_PROCESS_REQUEST
+        || action.type === actionTypes.RELOAD_APPLICATION_PROCESS_REQUEST
         || action.type === actionTypes.RENAME_APPLICATION_REQUEST) {
 
         const actionType = cloneDeep(state.actionType),
@@ -32,11 +32,11 @@ function application(state = initialState, action) {
 
     } else if (action.type === actionTypes.UPDATE_APPLICATION_SUCCESS
         || action.type === actionTypes.UPLOAD_APPLICATION_PACKAGE_SUCCESS
-        || action.type === actionTypes.START_APPLICATION_SUCCESS
-        || action.type === actionTypes.PAUSE_APPLICATION_SUCCESS
-        || action.type === actionTypes.RESTART_APPLICATION_SUCCESS
-        || action.type === actionTypes.STOP_APPLICATION_SUCCESS
-        || action.type === actionTypes.RELOAD_APPLICATION_SUCCESS
+        || action.type === actionTypes.START_APPLICATION_PROCESS_SUCCESS
+        || action.type === actionTypes.STOP_APPLICATION_PROCESS_SUCCESS
+        || action.type === actionTypes.RESTART_APPLICATION_PROCESS_SUCCESS
+        || action.type === actionTypes.DELETE_APPLICATION_PROCESS_SUCCESS
+        || action.type === actionTypes.RELOAD_APPLICATION_PROCESS_SUCCESS
         || action.type === actionTypes.RENAME_APPLICATION_SUCCESS) {
 
         const actionType = cloneDeep(state.actionType);
@@ -49,11 +49,11 @@ function application(state = initialState, action) {
 
     } else if (action.type === actionTypes.UPDATE_APPLICATION_FAILURE
         || action.type === actionTypes.UPLOAD_APPLICATION_PACKAGE_FAILURE
-        || action.type === actionTypes.START_APPLICATION_FAILURE
-        || action.type === actionTypes.PAUSE_APPLICATION_FAILURE
-        || action.type === actionTypes.RESTART_APPLICATION_FAILURE
-        || action.type === actionTypes.STOP_APPLICATION_FAILURE
-        || action.type === actionTypes.RELOAD_APPLICATION_FAILURE
+        || action.type === actionTypes.START_APPLICATION_PROCESS_FAILURE
+        || action.type === actionTypes.STOP_APPLICATION_PROCESS_FAILURE
+        || action.type === actionTypes.RESTART_APPLICATION_PROCESS_FAILURE
+        || action.type === actionTypes.DELETE_APPLICATION_PROCESS_FAILURE
+        || action.type === actionTypes.RELOAD_APPLICATION_PROCESS_FAILURE
         || action.type === actionTypes.RENAME_APPLICATION_FAILURE) {
 
         const actionType = cloneDeep(state.actionType),
