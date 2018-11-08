76712a6d67ff13558b2efda8e20282e913ac5ea8
liangxiaojun <fatalxiao@163.com>
2018-11-08 21:25:36 +0800

    update Applications

--- src/containers/app/pm/applications/Applications.js
+++ src/containers/app/pm/applications/Applications.js
@@ -2,9 +2,6 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import debounce from 'lodash/debounce';
-import eventsOn from 'dom-helpers/events/on';
-import eventsOff from 'dom-helpers/events/off';
 
 import {Responsive, WidthProvider} from 'react-grid-layout';
 import Application from './application/ApplicationCard';
@@ -19,6 +16,32 @@ class Applications extends Component {
         super(props);
     }
 
+    getLayout = col => {
+
+        const {data} = this.props;
+
+        return data ? data.map((item, index) => ({
+                i: '' + index,
+                x: index % col,
+                y: parseInt(index / col),
+                w: 1,
+                h: 1
+            }))
+            :
+            [];
+
+    };
+
+    getLayouts = () => {
+        return {
+            lg: this.getLayout(6),
+            md: this.getLayout(4),
+            sm: this.getLayout(3),
+            xs: this.getLayout(2),
+            xxs: this.getLayout(1)
+        };
+    };
+
     render() {
 
         const {data} = this.props;
@@ -31,10 +54,12 @@ class Applications extends Component {
                 <ResponsiveGridLayout className="applications-wrapper"
                                       breakpoints={{lg: 1200, md: 960, sm: 720, xs: 480, xxs: 0}}
                                       cols={{lg: 6, md: 4, sm: 3, xs: 2, xxs: 1}}
-                                      rowHeight={200}>
+                                      layouts={this.getLayouts()}
+                                      rowHeight={200}
+                                      isResizable={false}>
                     {
                         data && data.map((item, index) => item ?
-                            <Application key={index}
+                            <Application key={'' + index}
                                          data={item}/>
                             :
                             null
