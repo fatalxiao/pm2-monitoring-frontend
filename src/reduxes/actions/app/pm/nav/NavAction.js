ad949243661e411682dbdebd90a0323db562d76f
liangxiaojun <fatalxiao@163.com>
2018-11-09 09:53:22 +0800

    update NavAction

--- src/reduxes/actions/app/pm/nav/NavAction.js
+++ src/reduxes/actions/app/pm/nav/NavAction.js
@@ -1,1 +1,17 @@
 import * as actionTypes from 'reduxes/actionTypes';
+
+export const layoutGrid = () => dispatch => dispatch({
+    type: actionTypes.LAYOUT_GRID
+});
+
+export const layoutTable = () => dispatch => dispatch({
+    type: actionTypes.LAYOUT_TABLE
+});
+
+export const layoutGroup = () => dispatch => dispatch({
+    type: actionTypes.LAYOUT_GROUP
+});
+
+export const layoutUngroup = () => dispatch => dispatch({
+    type: actionTypes.LAYOUT_UNGROUP
+});
