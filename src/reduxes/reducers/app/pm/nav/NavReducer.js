7a32b42853e1d7ade6e1dbd600a98cce5ec4cf2c
liangxiaojun <fatalxiao@163.com>
2018-11-09 09:57:32 +0800

    update NavReducer

--- src/reduxes/reducers/app/pm/nav/NavReducer.js
+++ src/reduxes/reducers/app/pm/nav/NavReducer.js
@@ -1,11 +1,44 @@
 import * as actionTypes from 'reduxes/actionTypes';
 
-const initialState = {};
+const initialState = {
+    layout: actionTypes.LAYOUT_GRID,
+    isLayoutGroup: false
+};
 
 function nav(state = initialState, action) {
     switch (action.type) {
+
+        // switch layout to grid or table
+        case actionTypes.LAYOUT_GRID: {
+            return {
+                ...state,
+                layout: actionTypes.LAYOUT_GRID
+            };
+        }
+        case actionTypes.LAYOUT_TABLE: {
+            return {
+                ...state,
+                layout: actionTypes.LAYOUT_TABLE
+            };
+        }
+
+        // switch layout grouped ot ungrouped
+        case actionTypes.LAYOUT_GROUP: {
+            return {
+                ...state,
+                isLayoutGroup: true
+            };
+        }
+        case actionTypes.LAYOUT_UNGROUP: {
+            return {
+                ...state,
+                isLayoutGroup: false
+            };
+        }
+
         default:
             return state;
+
     }
 }
 
